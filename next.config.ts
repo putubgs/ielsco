f76a6c9b5d1128e175cfd5f07493a1e6ae34d010
Arbadza Rido Adzariyat <109972247+arbadzarido@users.noreply.github.com>
2026-01-31 19:07:03 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     // Tambahkan blok ini:
 env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: "https://hkubzamchahvdpvojepc.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrdWJ6YW1jaGFodmRwdm9qZXBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0ODQ0MjMsImV4cCI6MjA4MTA2MDQyM30.6CZZRCuV6jkFZ_dYq2gpHlDqX2n7Pcm_jxyMCjx_G4I",
   },
-
+  
   images: {
     remotePatterns: [
       {

bfd3011df33b9f9f83c60c05dbaa2b3115e3201a
Arbadza Rido Adzariyat <109972247+arbadzarido@users.noreply.github.com>
2026-02-21 02:41:55 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -62,6 +62,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|api/auth|api/test).*)',
+    '/((?!_next/static|_next/image|favicon.ico|api/auth|api/test|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
